50b580ba2a2065b3699cae0375389dd9db82047b
heshanu <heshanu97@gmail.com>
2023-12-26 14:51:27 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotfoundComponent } from './compoent/notfound/notfound.component';
 
 const routes: Routes = [
-  { path: 'category', 
-    loadChildren: () => import('./modules/categorymodule/categorymodule.module').then(m => m.CategorymoduleModule) 
-  }, {
-    path: 'login',
+   {
+    path: '',
     loadChildren: () => import('./modules/loginmodule/loginmodule.module').then(m => m.LoginmoduleModule)
+  },
+  {
+    path: 'category', 
+    loadChildren: () => import('./modules/categorymodule/categorymodule.module').then(m => m.CategorymoduleModule) 
   },
   {
     path:'**',component:NotfoundComponent
